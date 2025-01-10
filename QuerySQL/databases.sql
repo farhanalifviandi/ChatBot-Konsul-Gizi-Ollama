ee5a935a43a109553eb9c558ac1e802f597875ac
farhanalifviandi <130892023+farhanalifviandi@users.noreply.github.com>
2025-01-10 09:43:46 +0700

    Update databases.sql

--- QuerySQL/databases.sql
+++ QuerySQL/databases.sql
@@ -15,26 +15,3 @@ CREATE TABLE chat_history (
     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
-
--- SELECT * FROM 
-
--- DROP TABLE chat_history;
-
--- CREATE TABLE chat_history (
---     id INT AUTO_INCREMENT PRIMARY KEY,
---     user_id INT,
---     question TEXT,
---     response TEXT,
---     TIMESTAMP TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
---     FOREIGN KEY (user_id) REFERENCES users(id)
--- );
-
-
--- CREATE TABLE chat_history (
---     id INT AUTO_INCREMENT PRIMARY KEY,
---     user_input TEXT,
---     bot_response TEXT,
---     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
--- );
-
-
